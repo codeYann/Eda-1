55430fd23fde5a937e23eaff2026d5c1e72d84eb
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-11-17 11:06:37 -0300

    Heap test file

--- src/tests/Heap_test.c
+++ src/tests/Heap_test.c
@@ -1,9 +1,8 @@
 #include "../lib/heap.h"
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+void TestInsert() {
   Heap* h = CreateHeap(10);
-
   Insert(h, 33); 
   Insert(h, 39);
   Insert(h, 78);
@@ -13,12 +12,35 @@ int main(int argc, char* argv[]) {
   Insert(h, 28);
   Insert(h, 70);
 
-  printf("%d\n", h->list[0]);
-
   for(int i = 0; i < h->length; i++){
     printf(" %d ", h->list[i]);
   }
+}
 
+void TestRemove() {
+  Heap* h = CreateHeap(10);
+  Insert(h, 33); 
+  Insert(h, 39);
+  Insert(h, 78);
+  Insert(h, 60);
+  Insert(h, 95);
+  Insert(h, 66);
+  Insert(h, 28);
+  Insert(h, 70);
+
+  for(int i = 0; i < h->length; i++) {
+    printf(" %d ", h->list[i]);
+  }
+
+  printf("\n");
+  Remove(h);
+
+  for(int i = 0; i < h->length; i++) {
+    printf(" %d ", h->list[i]);
+  }
+}
+
+int main(int argc, char* argv[]) {
   return 0;
 }
 
