8d4b31ddd6982902d580042431a0e6ffa7e707ce
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-11-17 11:06:26 -0300

    Remove function declaration

--- src/lib/heap.h
+++ src/lib/heap.h
@@ -7,10 +7,10 @@ typedef struct {
   int length;
 } Heap;
 
-int GetParent(int index);
-int LeftChildren(int index);
-int RightChildren(int index);
 Heap *CreateHeap(int capacity);
 void maxHeapifyUp(Heap *heap, int index);
+void maxHeapifyDown(Heap *heap, int index);
 void Insert(Heap* heap, int element);
+int Remove(Heap* heap);
+
 #endif
