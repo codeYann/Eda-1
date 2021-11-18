6055d9632396b7ba286cfc1668da36f96a63fd6c
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-11-18 11:11:54 -0300

    Heap test file

--- src/tests/Heap_test.c
+++ src/tests/Heap_test.c
@@ -3,14 +3,13 @@
 
 void TestInsert() {
   Heap* h = CreateHeap(10);
-  Insert(h, 33); 
-  Insert(h, 39);
-  Insert(h, 78);
-  Insert(h, 60);
-  Insert(h, 95);
-  Insert(h, 66);
-  Insert(h, 28);
-  Insert(h, 70);
+
+  Insert(h, 4); 
+  Insert(h, 2);
+  Insert(h, 5);
+  Insert(h, 1);
+  Insert(h, 3);
+  Insert(h, 6);
 
   for(int i = 0; i < h->length; i++){
     printf(" %d ", h->list[i]);
@@ -40,7 +39,18 @@ void TestRemove() {
   }
 }
 
+void TestHeapify() {
+  int array[] = {4, 7, 3, 10, 2, 15, 1, 9, 6};
+  Heap* a = Heapify(array, 9);
+
+  for(int i = 0; i < a->length; i++) {
+    printf(" %d ", a->list[i]);
+  }
+
+}
+
 int main(int argc, char* argv[]) {
+  TestHeapify();
   return 0;
 }
 
