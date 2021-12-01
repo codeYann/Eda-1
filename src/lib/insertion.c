f27916d0b816f818bd10badc7c4caebcee3b3ff3
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-12-01 15:45:42 -0300

    Update data types int to unsigned long

--- src/lib/insertion.c
+++ src/lib/insertion.c
@@ -1,9 +1,9 @@
 #include "insertion.h"
 
-void InsertionSort(int list[], int n) {
-  for(int j = 1; j < n; j++) {
-    int key = list[j];
-    int i = j - 1;
+void InsertionSort(unsigned long* list, unsigned long n) {
+  for(unsigned long j = 1; j < n; j++) {
+    unsigned long key = list[j];
+    unsigned long i = j - 1;
     while(i >= 0 && list[i] > key) {
       list[i+1] = list[i];
       i -= 1;
