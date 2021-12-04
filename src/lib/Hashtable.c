b8b1eaf263df4103aa7ce101a9720e02e79cf457
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-12-04 15:17:57 -0300

    Change data type of hash table

--- src/lib/Hashtable.c
+++ src/lib/Hashtable.c
@@ -3,16 +3,20 @@
 #include <stdio.h>
 #include <math.h>
 
-unsigned int ModMethod(int key, int m) {
+unsigned long ModMethod(unsigned long key, unsigned long m) {
   return key % m;
 }
 
-unsigned int MultiplicationMethod(int key, int m) {
+unsigned long MultiplicationMethod(unsigned long key, unsigned long m) {
   const float A = 0.61231;
   return (m * fmodf(key * A, 1));
 }
 
-Node *CreateNode(int key, char *value) {
+unsigned long FoldMethod(unsigned long key, unsigned long m) {
+  return 1;
+}
+
+Node *CreateNode(unsigned long key, char *value) {
   Node* node = (Node*) malloc(sizeof(Node));
   node->value = value;
   node->key = key;
@@ -20,20 +24,22 @@ Node *CreateNode(int key, char *value) {
   return node;
 }
 
-HashTable* CreateHashTable(int capacity) {
+HashTable* CreateHashTable(unsigned long capacity) {
   HashTable* hashTable = (HashTable*) malloc(sizeof(HashTable));
   hashTable->capacity = capacity;
   hashTable->list = (Node**) malloc(sizeof(Node) * capacity);
+  hashTable->collision = 0;
   return hashTable;
 }
 
-void Append(HashTable *hashTable, int key, char *value, unsigned int (*HashMethod)(int key, int capacity)) {
-  unsigned int index = (*HashMethod)(key, hashTable->capacity);
+void Append(HashTable *hashTable, unsigned long key, char *value, unsigned long (*HashMethod)(unsigned long key, unsigned long capacity)) {
+  unsigned long index = (*HashMethod)(key, hashTable->capacity);
   Node* node = hashTable->list[index];
 
   if (node == NULL) {
     hashTable->list[index] = CreateNode(key, value);
   } else {
+    hashTable->collision += 1;
     while (node != NULL) {
       if (node->next == NULL) {
         node->next = CreateNode(key, value);
@@ -44,8 +50,8 @@ void Append(HashTable *hashTable, int key, char *value, unsigned int (*HashMetho
   }
 }
 
-char *Search(HashTable *hashTable, int key, unsigned int (*HashMethod)(int key, int capacity)) {
-  unsigned int index = (*HashMethod)(key, hashTable->capacity);
+char *Search(HashTable *hashTable, unsigned long key, unsigned long (*HashMethod)(unsigned long key, unsigned long capacity)) {
+  unsigned long index = (*HashMethod)(key, hashTable->capacity);
   Node* node = hashTable->list[index];
   while (node != NULL) {
     if (node->key == key) {
