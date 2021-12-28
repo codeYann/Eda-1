76a15f6e47d659218561bd38fdebc9cddd889ce5
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-12-28 15:11:15 -0300

    Formatting InsertionSort file

--- src/lib/insertion.c
+++ src/lib/insertion.c
@@ -1,10 +1,11 @@
 #include "insertion.h"
 
-void InsertionSort(unsigned long* list, unsigned long n) {
-  for(unsigned long j = 1; j < n; j++) {
+void InsertionSort(unsigned long *list, unsigned long n) 
+{
+  for (unsigned long j = 1; j < n; j++) {
     unsigned long key = list[j];
     unsigned long i = j - 1;
-    while(i >= 0 && list[i] > key) {
+    while (i >= 0 && list[i] > key) {
       list[i+1] = list[i];
       i -= 1;
     }
