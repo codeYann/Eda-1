2b00d07d8c3f5883f8cc387cce1fed9a8fa1acc3
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-01-12 11:52:38 -0300

    Formatting hashing test foldeR

--- src/tests/Hashing/Hashing.c
+++ src/tests/Hashing/Hashing.c
@@ -7,8 +7,9 @@
 #define LENGTH 0                     // Passe o length correto
 #define NAME "src/tests/Hashing/File.txt" // Passe o arquivo certo
 
-unsigned long *GenerateList(char *name, unsigned long size) {
-  unsigned long* list = (unsigned long *) malloc(sizeof(unsigned long) * size);
+unsigned long *GenerateList(char *name, unsigned long size) 
+{
+  unsigned long *list = (unsigned long *) malloc(sizeof(unsigned long) * size);
   FILE *file;
 
   if (!(file = fopen(name, "r"))) {
@@ -23,37 +24,39 @@ unsigned long *GenerateList(char *name, unsigned long size) {
   return list;
 }
 
-void Exec(int option) {
+void Exec(int option) 
+{
   HashTable *hash = CreateHashTable(SIZE);
   unsigned long *list = GenerateList(NAME, LENGTH);
   switch (option) {
-  case 1: {
-    for (unsigned long j = 0; j < LENGTH; j++) {
-      Append(hash, list[j], "Yan", &ModMethod);
+    case 1: {
+      for (unsigned long j = 0; j < LENGTH; j++) {
+        Append(hash, list[j], "Yan", &ModMethod);
+      }
+      printf("Número de colisões %lu\n", hash->collision);
     }
-    printf("Número de colisões %lu\n", hash->collision);
-  }
-  break;
-  case 2: {
-    for (unsigned long j = 0; j < LENGTH; j++) {
-      Append(hash, list[j], "Yan", &MultiplicationMethod);
+    break;
+    case 2: {
+      for (unsigned long j = 0; j < LENGTH; j++) {
+        Append(hash, list[j], "Yan", &MultiplicationMethod);
+      }
+      printf("Número de colisões %lu", hash->collision);
     }
-    printf("Número de colisões %lu", hash->collision);
-  }
-  break;
-  case 3: {
-    for (unsigned long j = 0; j < LENGTH; j++) {
-      Append(hash, list[j], "Yan", &FoldMethod);
+    break;
+    case 3: {
+      for (unsigned long j = 0; j < LENGTH; j++) {
+        Append(hash, list[j], "Yan", &FoldMethod);
+      }
+      printf("Número de colisões %lu", hash->collision);
     }
-    printf("Número de colisões %lu", hash->collision);
-  }
-  break;
+    break;
   }
   free(hash);
   free(list);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
   int i;
   puts("1 -> Método da Divisão");
   puts("2 -> Método da Multiplicação");
