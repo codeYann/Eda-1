df7d3b4eaf5469a2aa231325be9056644d6ecf56
codeYann <ccyan05@alu.ufc.br>
2022-02-19 13:51:08 -0300

    Refactoring insertion sort

--- src/lib/insertion.c
+++ src/lib/insertion.c
@@ -1,14 +1,13 @@
 #include "insertion.h"
 
-void insertion_sort(unsigned long *list, unsigned long n) 
-{
+void insertion_sort(unsigned long *list, unsigned long n) {
   for (unsigned long j = 1; j < n; j++) {
     unsigned long key = list[j];
     unsigned long i = j - 1;
     while (i >= 0 && list[i] > key) {
-      list[i+1] = list[i];
+      list[i + 1] = list[i];
       i -= 1;
     }
-    list[i+1] = key;
+    list[i + 1] = key;
   }
 }
