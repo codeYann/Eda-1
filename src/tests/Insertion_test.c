72b7caabbfa029de3b3f1fd6a8c26de655fd4e23
codeYann <ccyan05@alu.ufc.br>
2022-02-19 13:33:08 -0300

    Refactoring insertion sort test

--- src/tests/Insertion_test.c
+++ src/tests/Insertion_test.c
@@ -3,26 +3,24 @@
 
 #define LENGTH 14
 
-void TestInsertionSort() 
-{
-  unsigned long vec[] = {15, 45, 18, 80, 182, 84, 32, 23, 12, 3, 7, 8, 19, 1999};
+void test_insertion_sort() {
+  unsigned long vec[] = {15, 45, 18, 80, 182, 84, 32,
+                         23, 12, 3,  7,  8,   19, 1999};
 
   printf("Vetor pré Ordenação: ");
-  for(int i = 0; i < LENGTH; i++) {
+  for (int i = 0; i < LENGTH; i++) {
     printf(" %lu ", vec[i]);
   }
-  
+
   printf("\nVetor pós Ordenção: ");
 
-  InsertionSort(vec, LENGTH);
-  for(int i = 0; i < LENGTH; i++) {
+  insertion_sort(vec, LENGTH);
+  for (int i = 0; i < LENGTH; i++) {
     printf(" %lu ", vec[i]);
   }
-
 }
 
-int main(int argc, char **argv) 
-{
-  TestInsertionSort();
+int main(int argc, char **argv) {
+  test_insertion_sort();
   return 0;
 }
